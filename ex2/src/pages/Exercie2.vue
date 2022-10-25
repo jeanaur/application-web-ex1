c56dddd7c68f92d623146287ae4cb4f909c5418b
Jeanneret Aurélie <jeanaur@divtec.ch>
2022-10-25 16:13:53 +0200

    configu

--- ex2/src/pages/Exercie2.vue
+++ ex2/src/pages/Exercie2.vue
@@ -1,36 +1,6 @@
 <template>
-  <!--
-    Exercice 2 - Listes et composants Vue.js
-
-    1) Créez un tableau pour les plats,  que vous voyez sur la page.
-       Chaque objet du tableau doit contenir les propriété suivantes
-       image (url de l’image), nom, description et note (note de 1 à 5)
-
-    2) Créer une boucle v-for qui affiche ces aliments sur la page,
-       avec toutes les données provenant du tableau des aliments
-       (indice : pour l’image, à la place de l'attribut src,
-       utiliser la directive :src)
-
-    3) Ajoutez un de vos plats préférés à la liste
-       Vous pouvez trouver des images ici : https://imgur.com/
-
-    4) Créez un composant enfant pour les plats(components/Plat.vue)
-       et utilisez-le pour afficher chacun de vos plats
-
-    5) Dans le tableau des plats, ajoutez un identifiant unique (id)
-       à chaque plats.
-       Appliquez l'id à une propriété :key de votre boucle v-for.
-
-    6) Ajouter un lien ou un bouton supprimer à votre composant enfant Plat,
-       qui émettra un événement "supprimer" et passera l'id du plat.
-
-    7) Dans la page, écouter l'événement "supprimer" et supprimer le plat
-       du tableau des plats.
-
-  -->
   <q-page padding>
     <div class="q-pa-md row items-stretch">
-
       <div class="card shadow-1">
         <img src="https://i.imgur.com/0umadnY.jpg" width="198" height="180">
         <div class="card-content">
@@ -89,15 +59,18 @@
 </template>
 
 <script>
-export default {
-  data () {
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Exercice2Page',
+  data() {
     return {
       plats: [
         // vos objets plats
       ]
     }
   }
-}
+})
 </script>
 
 <style>
@@ -109,15 +82,19 @@ export default {
   margin-right: 10px;
   margin-bottom: 10px;
 }
+
 .card img {
   max-width: 100%;
 }
+
 .card-content {
   padding: 0 10px;
 }
+
 h1 {
   font-size: 23px;
 }
+
 p {
   font-size: 14px;
 }
