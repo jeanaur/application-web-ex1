669bf3c1ffaabab78f8f77cd0d542a2327e48d1f
Jeanneret Aurélie <jeanaur@divtec.ch>
2022-10-25 09:44:29 +0200

    ajout branche

---
+++ ex1/src/pages/Exercie1Page.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Exercie1Page.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
--- ex1/src/router/routes.js
+++ ex1/src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'ex1', component: () => import('pages/Exercie1Page.vue') }
     ]
   },
 
